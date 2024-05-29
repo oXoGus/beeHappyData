012efb171216fa88214bf32c5c0dc000db047b20
Zeffut <74143023+Zeffut@users.noreply.github.com>
2024-05-29 18:31:52 +0200

    Update Map.jsx

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = ({ coordinates }) => {
   return (
-    <MapContainer center={coordinates} zoom={13} style={{ height: "515px", width: "100%" }}>
+    <MapContainer center={coordinates} zoom={13}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
