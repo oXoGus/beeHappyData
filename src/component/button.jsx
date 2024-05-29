d31cb4a33203d13dee5955af9941c18112310ddb
oXoGus <mdintrat0205@gmail.com>
2024-05-29 19:13:48 +0200

    update

---
+++ src/component/button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Button = ({ as, children, filled, secondary, ...rest }) => {
+    const that = {
+      as
+    }
+    return (
+      <that.as className={`dir-control ${secondary ? 'dir-control--secondary' : ''} ${filled ? 'dir-control--filled' : ''}`} {...rest} >
+        {children}
+        <span/>
+        <span/>
+        <span/>
+        <span/>
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+      </that.as>
+    )
+  }
+  Button.defaultProps = {
+    as: 'button'
+  }
