5229e56501eac93b823ac144298c2fb431a89d30
mac de Mathis D <mathis.d@OrdinateMathisD.numericable.fr>
2024-05-29 17:03:25 +0200

    update

--- src/page/Panel.jsx
+++ src/page/Panel.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../component/navbar";
 import axios from "axios";
 import Map from "../component/Map";
-import reverseGeocode from "../component/adressMap";
 
 import '../styles/style.css';
 import '../styles/panel.css';
@@ -11,18 +10,11 @@ import WeightChart from "../component/WeightChart";
 
 function Panel() {
 
-    // useState pour afficher le graph que si il n'y a pas eu d'err dans la récuperation des donées 
     const [batteryData, setBatteryData] = useState(null);
-
-    //useState pour recuperé la valeur du menu déroulant 
     const [selectedOptionBattery, setSelectedOptionBattery] = useState('30');
-
     const [weightData, setWeightData] = useState(null);
     const [selectedOptionWeight, setSelectedOptionWeight] = useState('30');
-
-    const [coords, setCoords] = useState(null)
-    const [coordsLat, setCoordsLat] = useState(null)
-    const [coordsLng, setCoordsLng] = useState(null)
+    const [coords, setCoords] = useState(null);
     const [address, setAddress] = useState(null);
 
     const handleOptionChangeBattery = (event) => {
@@ -36,15 +28,13 @@ function Panel() {
     useEffect(() => {
         axios.get(`${window.location.origin}/api/get/battery/${selectedOptionBattery}`)
             .then(res => {
-                // Fonction pour formater les dates en français
                 function formatDate(dateString) {
                     const date = new Date(dateString);
                     const datePart = date.toLocaleDateString('fr-FR');
                     const timePart = date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
                     return `${datePart} à ${timePart}`;
                 }
-        
-                // Extraction des valeurs de battery et des dates formatées
+
                 const formattedData = {
                     labels: res.data.map(message => formatDate(message.messageDateTime)),
                     datasets: [{
@@ -52,7 +42,7 @@ function Panel() {
                         data: res.data.map(message => message.battery),
                         fill: false,
                         borderColor: 'rgb(75, 192, 192)',
-                        tension: 0.5 // Ajuster pour accentuer l'interpolation
+                        tension: 0.5
                     }]
                 };
 
@@ -61,22 +51,18 @@ function Panel() {
             .catch(err => {
                 console.error("Erreur lors du chargement des données de la batterie:", err);
             });
-    }, [selectedOptionBattery]); // Ecoute uniquement les changements de selectedOptionBattery
+    }, [selectedOptionBattery]);
 
-
-    // on réappel cette fonction des qu'il y a un changement de selectedOptionWeight
     useEffect(() => {
         axios.get(`${window.location.origin}/api/get/weight/${selectedOptionWeight}`)
             .then(res => {
-                // Fonction pour formater les dates en français
                 function formatDate(dateString) {
                     const date = new Date(dateString);
                     const datePart = date.toLocaleDateString('fr-FR');
                     const timePart = date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
                     return `${datePart} à ${timePart}`;
                 }
-        
-                // Extraction des valeurs de weight et des dates formatées
+
                 const formattedData = {
                     labels: res.data.map(message => formatDate(message.messageDateTime)),
                     datasets: [{
@@ -84,7 +70,7 @@ function Panel() {
                         data: res.data.map(message => message.weight),
                         fill: false,
                         borderColor: 'rgb(153, 102, 255)',
-                        tension: 0.5 // Ajuster pour accentuer l'interpolation
+                        tension: 0.5
                     }]
                 };
 
@@ -93,32 +79,23 @@ function Panel() {
             .catch(err => {
                 console.error("Erreur lors du chargement des données du poids", err);
             });
-    }, [selectedOptionWeight]); // Ecoute uniquement les changements de selectedOptionWeight
+    }, [selectedOptionWeight]);
 
     useEffect(() => {
         axios.get(`${window.location.origin}/api/get/coords/1`)
             .then(async res => {
-                console.log(res)
-                console.log(res.data[0].coordsLat)
-                setCoordsLat(res.data[0].coordsLat);
-                console.log(coordsLat)
-                setCoordsLng(res.data[0].coordsLng);
-                setCoords([coordsLat, coordsLng])
-                console.log(coords)
+                const coordLat = res.data[0].coordsLat;
+                const coordLng = res.data[0].coordsLng;
+                const coordinates = [coordLat, coordLng];
+                setCoords(coordinates);
 
-                axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${res.data[0].coordsLat}&lon=${res.data[0].coordsLng}&zoom=18&addressdetails=1`)
-                .then(res => {
-                    setAddress(res.data.display_name);
-                })
-                
+                const addressResponse = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coordLat}&lon=${coordLng}&zoom=18&addressdetails=1`);
+                setAddress(addressResponse.data.display_name);
             })
             .catch(err => {
-                console.error("Erreur lors du chargement des données des coords", err);
+                console.error("Erreur lors du chargement des coordonnées:", err);
             });
-    
-            
-    })
-        
+    }, []);
 
     return (
         <div>
@@ -140,26 +117,27 @@ function Panel() {
                         <div id="map">
                             <h1>Localisation</h1>
                             <div id="map_full">
-                                {coords && <Map coordinates={coords} />}
+                                {coords ? <Map coordinates={coords} /> : <p>Chargement de la carte...</p>}
                             </div>
                         </div>
                         <div className="infos">
                             <h1>Adresse</h1>
                             <div id="address" className="texte_info">
-                                {address && <p>{address}</p>}
+                                {address ? <p>{address}</p> : <p>Chargement de l'adresse...</p>}
                             </div>
                             <h1>Latitude</h1>
                             <div id="latitude" className="texte_info">
-                                {coordsLat && <p>{coordsLat}</p>}
+                                {coords ? <p>{coords[0]}</p> : <p>Chargement de la latitude...</p>}
                             </div>
                             <h1>Longitude</h1>
                             <div id="longitude" className="texte_info">
-                                {coordsLng && <p>{coordsLng}</p>}
+                                {coords ? <p>{coords[1]}</p> : <p>Chargement de la longitude...</p>}
                             </div>
-                            <a href='https://www.google.fr/maps/place/Emplacement+de+la+Ruche/@${coordsLat},${coordsLng},17z/' target="_blank">
-                                <button id="go_to">Aller a la ruche</button>
-                            </a>
-
+                            {coords && (
+                                <a href={`https://www.google.fr/maps/place/Emplacement+de+la+Ruche/@${coords[0]},${coords[1]},17z/`} target="_blank" rel="noopener noreferrer">
+                                    <button id="go_to">Aller à la ruche</button>
+                                </a>
+                            )}
                         </div>
                     </section>
                     <section className="batterie">
