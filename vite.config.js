e30ea0e2d4a91dff431e7530ed4897fc9a241d83
Mathis DINTRAT <thisma0206@gmail.com>
2024-05-30 10:28:38 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
-    //host: "172.16.42.6", 
+    host: "172.16.42.6", 
     //host: "192.168.1.25",
-    host: "localhost",
+    //host: "localhost",
     proxy: {
       "/api/": {
         target: 'http://localhost:5000', // URL du serveur backend
